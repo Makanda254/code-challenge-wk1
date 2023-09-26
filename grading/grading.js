0c2c0804f1382e03b3db40ac59e6264aa308b586
Victor John Makanda <victormakanda254@gmail.com>
2023-09-26 10:24:18 +0300

    Add comments

--- grading/grading.js
+++ grading/grading.js
@@ -2,23 +2,27 @@
  let mark;
 
  //Specifies the grading sacle
-
  function getGrade(mark){
+  // Checks whether the input score is greater than 70 and less than 100 returns grade "A"
    if(mark > 79 && mark <= 100){
      return 'A';
    } else if(mark >= 60 && mark<= 79){
+  // Checks whether the input score is between 60 and 79 and returns grade "B"
      return 'B';
    } else if(mark >= 50 && mark <= 59){
+  // Checks whether the input score is between 50 and 60 and returns grade "C"
      return 'C';
    } else if(mark >= 40 && mark <= 49){
+  // Checks whether the input score is between 40 and 50 and returns grade "D"
      return 'D';
    } else if(mark < 40){
+  // Checks whether the input score is less than 40 returns grade "E"
      return 'E';
    }
 
  }
 
- //Allows index.js to access the above function
+ //Allows the file to be accessible by the other file
  module.exports ={getGrade}
 
  
--- grading/index.js
+++ grading/index.js
@@ -1,16 +1,23 @@
+ //Imports grading function from grading.js
  const {getGrade} = require("./grading");
 const readline = require("readline");
 
 //Creates user interaction interface
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 //Prompts the user to enter marks for student
-
 rl.question("Enter the student's mark (between 0 and 100): ", (mark) => {
-  console.log(getGrade(mark));
+
+//Restricts user to enter marks between 0 and 100 else prints invalid input
+  if (isNaN(mark) || mark < 0 || mark > 100) {
+    console.log('Invalid input. Please enter a valid number between 0 and 100.')
+  }else {
+
+// Calls the grading function
+  console.log(getGrade(mark));// prints grade
+  }
   rl.close();
 });
